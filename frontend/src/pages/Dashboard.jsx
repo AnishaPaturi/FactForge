3e3b309d15736b5e062d20da910f5d63317e8c1b
Anishapaturi <paturi.anisha@gmail.com>
2026-03-22 07:07:58 +0530

    v1.41 : Fixed connection issue in dashboard page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -622,7 +622,9 @@ export default function Dashboard() {
 
     try {
       setStep(2)
-      const response = await fetch("http://localhost:8000/analyze", {
+      const API_BASE = import.meta.env.VITE_API_URL;
+
+      const response = await fetch(`${API_BASE}/analyze`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ text: input }),
