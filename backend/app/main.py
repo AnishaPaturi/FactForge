fd8016b447c305e88d0d2ea2f2f48460018138b8
Anishapaturi <paturi.anisha@gmail.com>
2026-03-22 23:04:46 +0530

    v1.60 : Adding DB to Login and signup pages

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,31 +1,73 @@
+# from fastapi import FastAPI
+# from app.api.routes import router
+# from app.models.db_models import Base
+# from app.db import Base, engine
+# from fastapi.middleware.cors import CORSMiddleware
+# from app.api import auth_routes
+# from fastapi.responses import StreamingResponse
+# from app.services.pdf_generator import generate_pdf
+# from app.api import auth_routes
+
+# app = FastAPI(title="FactForge API")
+
+# app.include_router(auth_routes.router)
+
+# Base.metadata.create_all(bind=engine)
+# app.include_router(auth_routes.router)
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],  # allow all for now
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+
+# app.include_router(router)
+
+# @app.post("/generate-pdf")
+# def generate_pdf_route(data: dict):
+#     pdf_buffer = generate_pdf(data)
+
+#     return StreamingResponse(
+#         pdf_buffer,
+#         media_type="application/pdf",
+#         headers={
+#             "Content-Disposition": "attachment; filename=FactForge_Report.pdf"
+#         }
+#     )
+
+
 from fastapi import FastAPI
-from app.api.routes import router
-from app.models.db_models import Base
-from app.db import Base, engine
 from fastapi.middleware.cors import CORSMiddleware
-from app.api import auth_routes
 from fastapi.responses import StreamingResponse
-from app.services.pdf_generator import generate_pdf
+
+from app.api.routes import router
 from app.api import auth_routes
+
+from app.db import Base, engine
+from app.services.pdf_generator import generate_pdf
 
 app = FastAPI(title="FactForge API")
 
-app.include_router(auth_routes.router)
-
+# ✅ create tables
 Base.metadata.create_all(bind=engine)
+
+# ✅ include routes
+app.include_router(router)
 app.include_router(auth_routes.router)
 
+# ✅ CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # allow all for now
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
-app.include_router(router)
-
+# ✅ PDF route
 @app.post("/generate-pdf")
 def generate_pdf_route(data: dict):
     pdf_buffer = generate_pdf(data)
